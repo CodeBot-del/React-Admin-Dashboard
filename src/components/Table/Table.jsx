675d1dae7a6baafc6ec30f035c93c72eff312c82
steve Manangu <stevenmanangu360@gmail.com>
2022-10-24 18:04:12 +0300

    table looking great

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -6,6 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import './Table.css'
 
 function createData(name, calories, fat, carbs, protein) {
 return { name, calories, fat, carbs, protein };
@@ -22,8 +23,10 @@ createData("Gingerbread", 356, 16.0, 49, 3.9),
 export default function BasicTable() {
 return (
     <div className="Table">
-        <h3>Recent Orders</h3>
-    <TableContainer component={Paper}>
+        <h3>Pending Orders</h3>
+    <TableContainer component={Paper}
+        style={{boxShadow: '0px 13px 20px 0px #50505029'}}
+    >
     <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
         <TableRow>
