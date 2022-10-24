4e678756974a2279f786f126bd32206c83f69a91
steve Manangu <stevenmanangu360@gmail.com>
2022-10-24 12:36:52 +0300

    rendering the cards to the dashboard

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { CardsData } from '../../Data/Data'
 import './Cards.css'
 
 const Cards = () => {
   return (
-    <div>Cards</div>
+    <div className="Cards">
+        {CardsData.map((card, id) =>{
+            return(
+                <div className="parentContainer">
+                    <Card/>
+                </div>
+            )
+        })}
+    </div>
   )
 }
 
