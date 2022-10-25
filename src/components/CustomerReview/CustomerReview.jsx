c88ecad226143b647682aa044cc37723f87c061d
steve Manangu <stevenmanangu360@gmail.com>
2022-10-25 10:36:37 +0300

    adding the customers served section

---
+++ src/components/CustomerReview/CustomerReview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CustomerReview = () => {
+    return (
+        <div className="CustomerReview">
+            Here is the customer review section
+        </div>
+    );
+};
+
+export default CustomerReview;
