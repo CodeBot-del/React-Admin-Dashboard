a63629872d008b2553e65b694417e60fcb86b5fe
steve Manangu <stevenmanangu360@gmail.com>
2022-10-25 10:27:44 +0300

    right side bar looking good

--- src/components/Updates/Updates.jsx
+++ src/components/Updates/Updates.jsx
@@ -12,12 +12,12 @@ const Updates = () => {
                     <div className="noti">
                         <div style={{marginBottom: '0.5rem'}}>
                             <span>{update.name}</span>
-                            <span>{update.noti}</span>
+                            <span> {update.noti}</span>
                         </div>
-                    </div>
-                    <div>
                         <span>{update.time}</span>
                     </div>
+                    
+                    
                 </div>
             )
         })}
